a652be9f86d6a19137bfa81e4ed63068fe383bf8
Chris Hoover <cmhoove14@gmail.com>
2018-08-10 09:24:07 -0700

    Age stratified stochastic model

--- R/Models/stochastic_age_stratified_model.R
+++ R/Models/stochastic_age_stratified_model.R
@@ -48,23 +48,23 @@ sfx <- function(x, p, t) {
       phi_Wt_adult = phi_Wk(W = Wt_adult, k = k3)  #Mating probability in treated adult population
       phi_Wu_adult = phi_Wk(W = Wu_adult, k = k4)  #Mating probability in untreated adult population
       
-    #Estimate number of mated female worms and their relative fecundity in each population
-      fem_Wt_SAC = 0.5*(Wt_SAC*phi_Wt_SAC) * f_Wgk(Wt_SAC, gam, k1)
+    #Estimate number of mated female worms in each population
+      fem_Wt_SAC = 0.5*(Wt_SAC*phi_Wt_SAC)
       
-      fem_Wu_SAC = 0.5*(Wu_SAC*phi_Wu_SAC) * f_Wgk(Wu_SAC, gam, k2)
+      fem_Wu_SAC = 0.5*(Wu_SAC*phi_Wu_SAC)
       
-      fem_Wt_adult = 0.5*(Wt_adult*phi_Wt_adult) * f_Wgk(Wt_adult, gam, k3) 
+      fem_Wt_adult = 0.5*(Wt_adult*phi_Wt_adult)
       
-      fem_Wu_adult = 0.5*(Wu_adult*phi_Wu_adult) * f_Wgk(Wu_adult, gam, k4)
+      fem_Wu_adult = 0.5*(Wu_adult*phi_Wu_adult)
       
       #multiplied by eggs per 10mL urine, mean urine produced by an individual in each population, number of inidivuals in the population, viability of eggs shed, and contamination factor (probability eggs shed end up in snail environment) below and summed across all populations to get miracidia (M) estimate used in estimating force of infection between humans and snails
  
   return(c(p$f_N * (1-N/p$C) * (S + E),   #Snail birth
            p$mu_N * S,                    #Susceptible snail death
-           p$beta * (1-exp(-(sum(fem_Wt_SAC * p$m * ((p$H*p$prop_SAC)*p$cvrg_SAC) * p$v * p$u_SAC * p$rho_SAC,
-                                 fem_Wu_SAC * p$m * ((p$H*p$prop_SAC)*(1-p$cvrg_SAC)) * p$v * p$u_SAC * p$rho_SAC,
-                                 fem_Wt_adult * p$m * ((p$H*p$prop_adult)*p$cvrg_adult) * p$v * p$u_adult * p$rho_adult,
-                                 fem_Wu_adult * p$m * ((p$H*p$prop_adult)*(1-p$cvrg_adult)) * p$v * p$u_adult * p$rho_adult))/N)) * S,  #Snail exposure
+           p$beta * (1-exp(-(sum(fem_Wt_SAC * f_Wgk(Wt_SAC, p$gam, k1) * p$m * ((p$H*p$prop_SAC)*p$cvrg_SAC) * p$v * p$u_SAC * p$rho_SAC,
+                                 fem_Wu_SAC * f_Wgk(Wu_SAC, p$gam, k2) * p$m * ((p$H*p$prop_SAC)*(1-p$cvrg_SAC)) * p$v * p$u_SAC * p$rho_SAC,
+                                 fem_Wt_adult * f_Wgk(Wu_adult, p$gam, k3) * p$m * ((p$H*p$prop_adult)*p$cvrg_adult) * p$v * p$u_adult * p$rho_adult,
+                                 fem_Wu_adult * f_Wgk(Wu_adult, p$gam, k4) * p$m * ((p$H*p$prop_adult)*(1-p$cvrg_adult)) * p$v * p$u_adult * p$rho_adult))/N)) * S,  #Snail exposure
            p$mu_N * E,                    #Exposed snail dies
            p$sigma * E,                   #Exposed snail becomes infected
            (p$mu_N + p$mu_I) * I,         #Infected snail dies
